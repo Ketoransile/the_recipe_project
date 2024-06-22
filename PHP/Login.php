90c7c3f0654f3d53923def6db35cb2bdab33d9a8
Israel Sol <isrucrasus10@gmail.com>
2024-06-22 17:06:21 +0300

    login php file for authenticating users

--- PHP/Login.php
+++ PHP/Login.php
@@ -5,4 +5,24 @@ header('Access-Control-Allow-Headers: token, Content-Type');
 header('Access-Control-Max-Age: 1728000');
 header('Content-Type: application/json'); // Correcting Content-Type to JSON
 
+$con=mysqli_connect("localhost","root","","user");
+// Check if the request method is POST
+if (isset($_POST)) {
+    // Initialize an array to store all POST data
+$data = file_get_contents("php://input");
+$user=json_decode($data,true);
+ $name=$user['firstName'];
+ $password=$user['password'];
+ $email=$user['email'];
+ $sql="select * from `sima` where email='$email' AND password='$password' AND user='$name'";
+ $res=mysqli_query($con,$sql);
+ $num=mysqli_num_rows($res);
+if($num>0){
+    echo json_encode("success");
+
+}
+else{
+    echo json_encode("wrong");
+}   
+}
 ?>
