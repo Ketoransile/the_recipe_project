ab409cbedcd14aee8e183614f2e1b581c5afca6f
Israel Sol <isrucrasus10@gmail.com>
2024-06-22 16:20:50 +0300

    added the login page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,28 @@ import Header from "./components/Header";
 import RecipeContainer from "./components/RecipeContainer";
 import About from "./components/About";
 import Footer from "./components/Footer";
+import Register from "./components/Auth/Signup";
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Login from "./components/Auth/Login";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header />
       <main>
         <RecipeContainer />
         <About />
       </main>
-      <Footer />
+      <Footer /> */}
+   <BrowserRouter>
+   
+   
+   <Routes>
+     <Route path='/Signup' element={<Register/>}></Route>
+     <Route path='/Login' element={<Login/>}></Route>
+     <Route path='/' element={<RecipeContainer/>}></Route>
+   </Routes>
+  </BrowserRouter>      
     </div>
   );
 }
