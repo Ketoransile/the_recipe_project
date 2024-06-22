ebf25962f4c03de58fa8be82bd1513f142151c8c
Israel Sol <isrucrasus10@gmail.com>
2024-06-22 17:02:04 +0300

    signup.php for registrating users

--- PHP/Signup.php
+++ PHP/Signup.php
@@ -5,4 +5,23 @@ header('Access-Control-Allow-Headers: token, Content-Type');
 header('Access-Control-Max-Age: 1728000');
 header('Content-Type: application/json'); // Correcting Content-Type to JSON
 
+$con=mysqli_connect("localhost","root","","user");
+// Check if the request method is POST
+if (isset($_POST)) {
+    // Initialize an array to store all POST data
+$data = file_get_contents("php://input");
+$user=json_decode($data,true);
+ $name=$user['firstName'];
+ $password=$user['password'];
+ $email=$user['email'];
+$sql="insert into sima (`user`,`email`,`password`) values ('$name','$email','$password')";
+ $res=mysqli_query($con,$sql);
+if ($res){
+    echo  json_encode("hola");
+ }
+
+else{
+    echo json_decode("a problem occured");
+}
+}
 ?>
